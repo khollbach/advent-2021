7ed815c01e588cc1632c2bde5fe8ee3bf03f53e8
Kevan Hollbach <khollbach@gmail.com>
2021-12-01 15:21:21 -0500

    Copy project structure from last year

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+mod solutions;
+
+pub fn solve(day: u32) {
+    assert!(1 <= day && day <= 25);
+
+    use solutions::*;
+    match day {
+        1 => day1::main(),
+        _ => panic!("Not yet implemented: Day {}", day),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use std::env;
+use std::process;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args = env::args();
+    let prog_name = args.next().unwrap();
+    let args: Vec<_> = args.collect();
+
+    let usage = || {
+        eprintln!();
+        eprint_usage(&prog_name);
+        process::exit(1)
+    };
+
+    if args.len() != 1 {
+        eprintln!("Expected 1 argument, got {}.", args.len());
+        usage();
+    }
+
+    let day: u32 = match args[0].parse() {
+        Ok(n) => if 1 <= n && n <= 25 {
+            n
+        } else {
+            eprintln!("Not a number from 1 through 25: {}", n);
+            usage()
+        }
+        Err(_) => {
+            eprintln!("Not a u32: {}", &args[0]);
+            usage()
+        }
+    };
+
+    advent_2021::solve(day);
+}
+
+fn eprint_usage(prog_name: &str) {
+    eprintln!("Usage: `{} <num>`\nwhere <num> is a number from 1 through 25.", prog_name);
 }
