4c05f72dc3b8a1206943f14ece31950f234ad868
Kevan Hollbach <khollbach@gmail.com>
2021-12-01 15:21:31 -0500

    Day 1 part 1

--- src/solutions/day1.rs
+++ src/solutions/day1.rs
@@ -1,3 +1,24 @@
+use std::io;
+use std::io::BufRead;
+
+fn read_input(input: impl BufRead) -> Vec<u32> {
+    input.lines().map(|line| line.unwrap().parse().unwrap()).collect()
+}
+
 pub fn main() {
-    println!("Hello from Day 1");
+    let nums = read_input(io::stdin().lock());
+
+    println!("{}", part_1(&nums));
+}
+
+fn part_1(nums: &[u32]) -> u32 {
+    let mut count = 0;
+
+    for i in 1..nums.len() {
+        if nums[i-1] < nums[i] {
+            count += 1;
+        }
+    }
+
+    count
 }
