c5248247253aa63b3404ebb6c4042ac1a3265fe9
Kevan Hollbach <khollbach@gmail.com>
2021-12-02 16:25:14 -0500

    2-2

--- src/solutions/day2/mod.rs
+++ src/solutions/day2/mod.rs
@@ -14,6 +14,7 @@ pub fn main() {
     let commands = read_input(io::stdin().lock());
 
     println!("{}", part_1(&commands));
+    println!("{}", part_2(&commands));
 }
 
 fn part_1(commands: &[(Dir, u32)]) -> u32 {
@@ -30,6 +31,29 @@ fn part_1(commands: &[(Dir, u32)]) -> u32 {
             }
             Up => {
                 depth -= n;
+            }
+        }
+    }
+
+    depth * distance
+}
+
+fn part_2(commands: &[(Dir, u32)]) -> u32 {
+    let mut aim = 0;
+    let mut depth = 0;
+    let mut distance = 0;
+
+    for (d, n) in commands {
+        match d {
+            Forward => {
+                distance += n;
+                depth += aim * n;
+            }
+            Down => {
+                aim += n;
+            }
+            Up => {
+                aim -= n;
             }
         }
     }
