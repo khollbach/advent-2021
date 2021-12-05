fa0042f5f1989c1b1f91c6d4a7000c0c98f03b77
Kevan Hollbach <khollbach@gmail.com>
2021-12-04 21:19:46 -0500

    Day 2: better workaround for E0716

--- src/solutions/day2/input.rs
+++ src/solutions/day2/input.rs
@@ -4,9 +4,7 @@ use crate::solutions::day2::Dir::{Down, Forward, Up};
 
 pub fn read_input(input: impl BufRead) -> Vec<(Dir, u32)> {
     input.lines().map(|line| {
-        let line = line.unwrap();
-
-        let mut words = line.split(' ');
+        let mut words = line.as_ref().unwrap().split(' ');
         let dir = Dir::new(words.next().unwrap());
         let num = words.next().unwrap().parse().unwrap();
         assert!(words.next().is_none());
