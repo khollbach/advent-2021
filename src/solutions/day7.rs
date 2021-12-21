e50a29b9902e7e2e04a4fc5fe58e8f507590e9ca
Kevan Hollbach <khollbach@gmail.com>
2021-12-21 15:08:47 -0500

    7-2

--- src/solutions/day7.rs
+++ src/solutions/day7.rs
@@ -11,12 +11,31 @@ fn read_input(input: impl BufRead) -> Vec<i32> {
 pub fn main() {
     let positions = read_input(io::stdin().lock());
 
+    println!("{}", min_fuel(&positions, |x| x));
+    println!("{}", min_fuel(&positions, triangular_number));
+}
+
+/// `cost_fn(d)` is how much fuel it costs to move distance d >= 0.
+///
+/// We want to find a `center` position that minimizes the total fuel spent
+/// for all crabs to move to that position.
+fn min_fuel(positions: &[i32], cost_fn: impl Fn(u32) -> u32) -> u32 {
     let min = *positions.iter().min().unwrap();
     let max = *positions.iter().max().unwrap();
 
-    let ans: i32 = (min..=max).map(|center| {
-        positions.iter().map(|&p| (center - p).abs()).sum()
-    }).min().unwrap();
+    // For each possible center point:
+    (min..=max).map(|center| {
+        // Add up all the fuel costs.
+        positions.iter().map(|&p| {
+            let dist = (center - p).abs() as u32;
+            cost_fn(dist)
+        }).sum()
+    }).min().unwrap()
+}
 
-    dbg!(ans);
+/// 1 + 2 + ... + n
+///
+/// We could make this O(1) with a closed formula, but we don't need to for this problem.
+fn triangular_number(n: u32) -> u32 {
+    (1..=n).sum()
 }
