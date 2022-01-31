50f9c508f642d7ddfd9389f815e1ae052d5f2cf5
Kevan Hollbach <khollbach@gmail.com>
2022-01-30 21:13:07 -0500

    20-1: generalize approach
    It worked out pretty nicely.

--- src/solutions/day20.rs
+++ src/solutions/day20.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::io;
 use std::io::BufRead;
 
@@ -6,50 +6,71 @@ fn read_input(input: impl BufRead) -> (Mask, Grid) {
     let mut lines = input.lines().map(Result::unwrap);
 
     let line = lines.next().unwrap();
-    let mask: Vec<_> = line.chars().map(char_to_pixel).collect();
+    let mask: Vec<_> = line.chars().map(Pixel::from_char).collect();
     let mask: Mask = mask.try_into().expect("Wrong number of pixels in mask.");
 
     assert_eq!(lines.next().unwrap(), "");
 
-    let mut lit_pixels = HashSet::new();
-    for (i, line) in lines.enumerate() {
-        for (j, c) in line.chars().enumerate() {
-            if char_to_pixel(c) {
-                lit_pixels.insert((i as i32, j as i32));
-            }
-        }
-    }
-    let grid = Grid { lit_pixels };
+    let grid = read_grid(lines);
 
     (mask, grid)
 }
 
-fn char_to_pixel(c: char) -> bool {
-    match c {
-        '.' => false,
-        '#' => true,
-        _ => panic!("Invalid pixel {c}"),
+fn read_grid(lines: impl Iterator<Item=String>) -> Grid {
+    let mut main_pixels = HashMap::new();
+
+    for (i, line) in lines.enumerate() {
+        for (j, c) in line.chars().enumerate() {
+            main_pixels.insert((i as i32, j as i32), Pixel::from_char(c));
+        }
+    }
+
+    Grid {
+        main_pixels,
+        background_color: Pixel::Off,
     }
 }
 
-type Mask = [bool; 2usize.pow(9)];
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+enum Pixel {
+    Off,
+    On,
+}
+
+impl Pixel {
+    fn from_char(c: char) -> Pixel {
+        match c {
+            '.' => Pixel::Off,
+            '#' => Pixel::On,
+            _ => panic!("Invalid pixel: {c}"),
+        }
+    }
+}
+
+type Mask = [Pixel; 2usize.pow(9)];
 
 struct Grid {
-    lit_pixels: HashSet<Point>,
+    main_pixels: HashMap<Point, Pixel>,
+    background_color: Pixel,
 }
 
 type Point = (i32, i32);
 
 impl Grid {
     fn enhance(&mut self, mask: &Mask) {
-        let relevant_points: HashSet<_> = self.lit_pixels.iter().flat_map(|&p| {
+        let relevant_points: HashSet<_> = self.main_pixels.keys().flat_map(|&p| {
             neighborhood(p)
         }).collect();
 
-        self.lit_pixels = relevant_points.into_iter().filter(|&p| {
+        self.main_pixels = relevant_points.into_iter().map(|p| {
             let idx = self.get_neighborhood(p);
-            mask[idx]
+            (p, mask[idx])
         }).collect();
+
+        self.background_color = match self.background_color {
+            Pixel::Off => mask[0],
+            Pixel::On => *mask.last().unwrap(),
+        };
     }
 
     /// Get the surrounding pixels, and interpret them as a binary number.
@@ -57,7 +78,8 @@ impl Grid {
         let mut answer = 0;
         for p2 in neighborhood(p) {
             answer <<= 1;
-            answer |= self.lit_pixels.contains(&p2) as usize;
+            let pixel = *self.main_pixels.get(&p2).unwrap_or(&self.background_color);
+            answer |= pixel as usize;
         }
         answer
     }
@@ -86,11 +108,11 @@ const SAMPLE: &str = "\
 
 pub fn main() {
     let (mask, mut grid) = read_input(io::stdin().lock());
-    // let (mask, mut grid) = read_input(SAMPLE.as_bytes()); // works
+    // let (mask, mut grid) = read_input(SAMPLE.as_bytes()); // still works
 
     for _ in 0..2 {
         grid.enhance(&mask);
     }
 
-    dbg!(grid.lit_pixels.len());
+    dbg!(grid.main_pixels.values().filter(|&&p| p == Pixel::On).count());
 }
